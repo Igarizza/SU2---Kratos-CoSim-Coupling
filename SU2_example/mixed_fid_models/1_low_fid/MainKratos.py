8af7583ef13e84d36ecde1b8e7b302bba031a468
IAntonau <ihar.antonau@tu-braunschweig.de>
2024-03-06 16:20:38 +0100

    small comment

--- SU2_example/mixed_fid_models/1_low_fid/MainKratos.py
+++ SU2_example/mixed_fid_models/1_low_fid/MainKratos.py
@@ -1,12 +1,5 @@
-from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
-
 import KratosMultiphysics
 from KratosMultiphysics.CoSimulationApplication.co_simulation_analysis import CoSimulationAnalysis
-import sys
-import time
-import os
-
-# os.chdir("/localdata/CoSim/low_fid_4")
 
 if __name__ == "__main__":
 
